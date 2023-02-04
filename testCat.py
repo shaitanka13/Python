4ef133ea3814ab05d17f9f0ba927167ce825e4f5
shaitanka13 <122038076+shaitanka13@users.noreply.github.com>
2023-02-04 17:22:47 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,14 @@
+from cat import Cat
+
+pet1 = Cat("Барон", "мальчик", 2)
+pet2 = Cat("Сэм", "мальчик", 2)
+
+print("имя=", pet1.name)
+print("пол=", pet1.gender)
+print("возраст=", pet1.age)
+
+print()
+
+print("имя=", pet2.name)
+print("пол=", pet2.gender)
+print("возраст=", pet2.age)
